f0760ab276d2f142d0254eba26a76d916c0bccea
lamsyah <96395805+lamsyahbilal@users.noreply.github.com>
2023-05-25 11:39:09 +0100

    Add files via upload

---
+++ navebar/main.js
@@ -0,0 +1,10 @@
+let navigation_bar=document.querySelector(".navbar");
+let menu_btn= document.querySelector(".menu-btn");
+let close_btn=document.querySelector(".close-btn");
+menu_btn.addEventListener("click", function(){
+    navigation_bar.classList.add("active2");
+});
+
+close_btn.addEventListener("click", function(){
+    navigation_bar.classList.remove("active2");
+});
